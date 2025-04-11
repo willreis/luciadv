d3be128389790cc2e2896fc9156b1ee5ed0024c1
willreis <william.reis.silva@gmail.com>
2025-04-11 13:55:40 -0300

    ajuste

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAuth } from "@/hooks/useAuth";
+import { useAuth } from "../../../../hooks/useAuth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
